7231b2f790e84a69fc869b33e4b311ffb83fc769
david <davidsanchezdiaz72@gmail.com>
2023-12-06 19:19:34 +0100

    Obteniendo personajes de la Api

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,37 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        Characters: [],
+        CharactersFilter: []
+    },
+
+    getters: {
+    },
+
+    mutations: {
+        setCharacters(state, payload) {
+          state.Characters = payload
+        },
+
+        setCharactersFilter(state, payload) {
+          state.CharactersFilter = payload
+        }
+
+    },
+
+    actions: {
+      async getCharacters({commit}) {
+        try{
+          const response = await fetch('https://rickandmortyapi.com/api/character')
+          const data = await response.json()
+          console.log(data)
+        } catch(error){
+            console.error(error)
+        }
+      }
+    },
+    
+    modules: {
+    }
 })
